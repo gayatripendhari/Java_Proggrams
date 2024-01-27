56a1382d01c0943f501ddbcd039263272be87e23
gayatripendhari <gayatriapendhari123@gmail.com>
2024-01-27 22:13:52 +0530

    Third Commit

---
+++ Day05/association/hasademo/HasADemo.java
@@ -0,0 +1,12 @@
+package Day05.association.hasademo;
+
+public class HasADemo {
+	public static void main(String[] args) {
+		
+		
+		Address addr=new Address(504,"greencity","Mumbai",421501);
+		Student s1=new Student("Sakshi",24,addr);
+		s1.dispInfo();
+	}
+    
+}
