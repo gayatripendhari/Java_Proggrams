a224295f204644b2e45d7a3dde03498de88e35d4
gayatripendhari <gayatriapendhari123@gmail.com>
2024-01-28 21:33:40 +0530

    fourth commit

---
+++ Day06/staticvariabledemo/Demo.java
@@ -0,0 +1,16 @@
+package Day06.staticvariabledemo;
+
+public class Demo {
+	public static void main(String[] args) {
+		Employee e1=new Employee("Kajal",10);
+		Employee e2=new Employee("Shubham",11);
+		Employee e3=new Employee("Madhuri",12);
+		
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+		
+
+	}
+   
+}
